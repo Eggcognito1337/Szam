5c4d850778d6ec3d9facca77ccef6e2363844bf4
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 15:51:29 +0200

    10 szám

--- Szam/Program.cs
+++ Szam/Program.cs
@@ -10,6 +10,21 @@ namespace Szam
     {
         static void Main(string[] args)
         {
+            List<int> l = new List<int>();
+            int szam = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Adjon meg egy számot: ");
+                szam = Convert.ToInt32(Console.ReadLine());
+                l.Add(szam);
+            }
+
+            int max = l.Max();
+            Console.WriteLine("Legnagyobb szám: " + max);
+
+
+            Console.ReadKey();
         }
     }
 }
